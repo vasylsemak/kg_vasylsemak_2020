b0e2fd10c5394319c17f6b7d70a71a4b3989f094
Vasyl Semak <vasylsmk1401@gmail.com>
2021-04-29 12:50:39 -0400

    remove comments, modify readme

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 const converter = require('number-to-words')  /* npm package */
 const commandLineArgs = process.argv.slice(2)
 
-// Split every number into digits array
-// Convert digits to strings using toWords() method
-// Upper case every first letter of the string
-// Join every string DIGIT of inner array with no space
-// Join every string NUMBER into String of Numbers
-
 const integersToStrings = args => [...args]
   .map(num => num
     .split('')
     .map(digit => {
-      let digitStr = converter.toWords(digit)
+      const digitStr = converter.toWords(digit)
       return digitStr[0].toUpperCase() + digitStr.slice(1)
     })
     .join('')
