eee96eb10ba85390d687344b936eedf6496cbbfc
Vasyl Semak <vasylsmk1401@gmail.com>
2021-04-29 12:24:28 -0400

    modify: edit code base to make it shorter

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const converter = require('number-to-words'); /* npm package */
-const commandLineArgs = process.argv.slice(2);
+const converter = require('number-to-words')  /* npm package */
+const commandLineArgs = process.argv.slice(2)
 
 // Split every number into digits array
 // Convert digits to strings using toWords() method
@@ -7,22 +7,17 @@ const commandLineArgs = process.argv.slice(2);
 // Join every string DIGIT of inner array with no space
 // Join every string NUMBER into String of Numbers
 
-function integersToStrings(args) {
-  const numbersArray = [...args];
+const integersToStrings = args => [...args]
+  .map(num => num
+    .split('')
+    .map(digit => {
+      let digitStr = converter.toWords(digit)
+      return digitStr[0].toUpperCase() + digitStr.slice(1)
+    })
+    .join('')
+  )
+  .join(', ')
 
-  const numberStringsArr = numbersArray.map(num => {
-    const digitStringsArr = num.split('').map(digit => {
-      const digitWorld = converter.toWords(digit);
-
-      return digitWorld[0].toUpperCase() + digitWorld.slice(1);
-    });
-
-    return digitStringsArr.join('');
-  })
-  const numbersString = numberStringsArr.join(', ');
-  console.log(numbersString);
-
-  return numbersString;
-}
-
-integersToStrings(commandLineArgs);
+console.log(
+  integersToStrings(commandLineArgs)
+)
