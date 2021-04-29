5310f79498daabe318cb8a642985461d800d409e
Vasyl Semak <vasylsmk1401@gmail.com>
2021-04-29 10:24:20 -0400

    convert every number to digits array using split() method; convert every digit to string using 'number-to-words' library

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const converter = require('number-to-words');
-
-let x = converter.toWords(13); // => “thirteen”
+const converter = require('number-to-words'); /* npm package */
 const commandLineArgs = process.argv.slice(2);
+// let x = converter.toWords(13);
 
-console.log(x, commandLineArgs);
+function integersToStrings(args) {
+  const numbersArray = [...args];
+
+  return numbersArray.map(num => {
+    // Convert every number to digits array
+    const digitsArr = num.split('').map(digit => converter.toWords(digit))
+    console.log('NUM ->', digitsArr)
+  })
+
+}
+
+integersToStrings(commandLineArgs);
